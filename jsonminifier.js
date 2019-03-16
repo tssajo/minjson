47c443fdf9c3a1516539657ac45368db2085c722
tssajo <tssajo@gmail.com>
2019-03-16 11:16:41 +0100

    minjson 0.0.3 was broken because UglifyJS 3 is different than UglifyJS 2. This is fixed now

--- jsonminifier.js
+++ jsonminifier.js
@@ -4,16 +4,9 @@ var UglifyJS = require('uglify-js');
 
 function jsonminifier(input, options) {
   var tmpVar = 'globalMinifyVariableByTSS',
-      ast = UglifyJS.parse(tmpVar + ' = ' + input);
-  if (options.compress) {
-    ast.figure_out_scope();
-    var compressor = UglifyJS.Compressor({booleans: false});
-    ast = ast.transform(compressor);
-  }
-  var re = new RegExp('^' + tmpVar + '\\s*=\\s*([^]*?);?$'),
-      stream = UglifyJS.OutputStream(options.output);
-  ast.print(stream);
-  return stream.toString().replace(re, '$1');
+      re = new RegExp('^' + tmpVar + '\\s*=\\s*([^]*?);?$'),
+      result = UglifyJS.minify(tmpVar + ' = ' + input, options);
+  return result.code.replace(re, '$1');
 }
 
 module.exports = jsonminifier;
